be2809ee13ea7f60371ba26064ea7443ef00c6c3
kwood <155027796+qkewq@users.noreply.github.com>
2025-09-26 22:38:25 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,17 +4,8 @@ apt install openssh-server -y
 systemctl start sshd
 
 useradd -m -s /bin/bash ssh-user
-while [ 1 ]; do
-  read -s -p "Password: " pw
-  read -s -p "Confirm: " cpw
-  if [ $pw == $cpw ]; then
-    break
-  else
-    echo "Passwords do not match"
-  fi
-done
 
-echo ssh-user:$pw | chpasswd
+echo ssh-user:password | chpasswd
 mkdir /home/ssh-user/.ssh
 touch /home/ssh-user/.ssh/authorized_keys
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCxm2qvXKjqVOqytO3r8MzlAoGVUP8AS31PaCkkpi7piFNhvRTQARDXoGdg5CRjT/tWvKzpufao9glVzTyKzOacS+UHJanbUIC1zqSaWeH4aITLcmqnpb+BmvtU/eGhx/pQJHPVraxv/Tls4Cmt4ptHBJXUx0S+ldFp6YCqFxMpKIe6Mx+DKFGyL0Eisn9PbDqQK10CyMcL6PIftdp42Q8Zm3J2F4KoQGlR6Ba02SnJN8c1H9o+dDJh3pjR5m5SJpRL1/Lk+DBnk/B/xC2CYFLtT4EBVVWD3u5bonuWcrTXICXYPPoHcl/PSEnYpnLv8QuYVrq8yIW9oCp+RfbtCv0DrO9gSFXa6/mWzs1jMXVYpxizOeJgIzBQxMC52oiyFeZIBdsfrcVvRdh4WrRWKm8N04wftfkukwTfuLvuos729ydBO+81xtJ9vk3cnc+uOmy/0kFRJ0ad2eJY464eFTss03dAm4kqm6Q91CsKTJdlkBxXM6za+zRn6MnTDqMuLJU=" > /home/ssh-user/.ssh/authorized_keys
@@ -28,7 +19,7 @@ apt install vsftpd -y
 
 sed -i 's/anonymous_enable=NO/anonymous_enable=YES/g' /etc/vsftpd.conf
 touch /srv/ftp/iloveftp.txt
-echo iloveftp > iloveftp.txt
+echo iloveftp > /srv/ftp/iloveftp.txt
 systemctl restart vsftpd
 echo "FTP setup"
 
@@ -40,8 +31,8 @@ echo "Apache setup"
 
 apt install bind9 dnsutils -y
 
-echo -e "zone \"local\" {type master\;file \"/etc/bind/db.local\;\"\;} >> /etc/bind/named.conf.local"
-echo -e "test\tIN\tA\t10.10.10.10"
+echo -e "zone \"local\" {type master\;file \"/etc/bind/db.local\;\"\;}" >> /etc/bind/named.conf.local
+echo -e "test\tIN\tA\t10.10.10.10" >> /etc/bind/db.local
 systemctl restart bind9
 echo "DNS setup"
 
